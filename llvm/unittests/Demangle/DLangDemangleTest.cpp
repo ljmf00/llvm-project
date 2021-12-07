ljmf00/lldb-d

[Demangle] Add support for D special identifiers
Signed-off-by: Luís Ferreira <contact@lsferreira.net>

llvm/unittests/Demangle/DLangDemangleTest.cpp
@@ -36,5 +36,13 @@ INSTANTIATE_TEST_SUITE_P(
         std::make_pair("_D8demangle4testZ", "demangle.test"),
         std::make_pair("_D8demangle4test5test2Z", "demangle.test.test2"),
         std::make_pair("_D8demangle9anonymous0Z", "demangle.anonymous"),
-        std::make_pair("_D8demangle9anonymous03fooZ",
-                       "demangle.anonymous.foo")));
+        std::make_pair("_D8demangle9anonymous03fooZ", "demangle.anonymous.foo"),
+        std::make_pair("_D8demangle4test6__initZ",
+                       "initializer for demangle.test"),
+        std::make_pair("_D8demangle4test6__vtblZ", "vtable for demangle.test"),
+        std::make_pair("_D8demangle4test7__ClassZ",
+                       "ClassInfo for demangle.test"),
+        std::make_pair("_D8demangle4test11__InterfaceZ",
+                       "Interface for demangle.test"),
+        std::make_pair("_D8demangle4test12__ModuleInfoZ",
+                       "ModuleInfo for demangle.test")));

ljmf00/llvm-project

llvm/unittests/Demangle/DLangDemangleTest.cpp
@@ -36,4 +36,13 @@ INSTANTIATE_TEST_SUITE_P(
         std::make_pair("_D8demangle4testZ", "demangle.test"),
         std::make_pair("_D8demangle4test5test2Z", "demangle.test.test2"),
         std::make_pair("_D8demangle4test0Z", "demangle.test"),
-        std::make_pair("_D8demangle4test03fooZ", "demangle.test.foo")));
+        std::make_pair("_D8demangle4test03fooZ", "demangle.test.foo"),
+        std::make_pair("_D8demangle4test6__initZ",
+                       "initializer for demangle.test"),
+        std::make_pair("_D8demangle4test6__vtblZ", "vtable for demangle.test"),
+        std::make_pair("_D8demangle4test7__ClassZ",
+                       "ClassInfo for demangle.test"),
+        std::make_pair("_D8demangle4test11__InterfaceZ",
+                       "Interface for demangle.test"),
+        std::make_pair("_D8demangle4test12__ModuleInfoZ",
+                       "ModuleInfo for demangle.test")));